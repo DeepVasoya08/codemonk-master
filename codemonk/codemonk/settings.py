7a6672fe23f3141f0b84bc1e48b555c1f2f57dce
Deep Vasoya <deep.vasoya@bosctechlabs.com>
2023-04-21 14:39:06 +0530

    changed DB host

--- codemonk/codemonk/settings.py
+++ codemonk/codemonk/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         "NAME": "codemonk",
         "USER": "postgres",
         "PASSWORD": "7984/*",
-        "HOST": "127.0.0.1",
+        "HOST": "db",
         "PORT": "5432",
     }
 }
